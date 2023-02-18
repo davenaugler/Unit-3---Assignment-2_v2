219a246b887bda1816e755e86411d3ebdd74640f
Dave Naugler <davenaugler@gmail.com>
2023-02-18 08:08:09 -0700

    Cleaned up my code and if else structure
    After code review I see where I can be more explicit in my validation, variable naming, and ditching a few un needed lines of code.

--- src/com/coderscampus/objects/Guess.java
+++ src/com/coderscampus/objects/Guess.java
@@ -8,37 +8,47 @@ public class Guess {
 	String guessNumber() {
 
 		Random random = new Random();
-		int theRandomNumber;
+		int randomNumber = 1 + random.nextInt(100);
 		int numChancesLeft = 5;
 		Scanner scanner = new Scanner(System.in);
 
-		theRandomNumber = 1 + random.nextInt(100);
 //		Line below is for testing purposes on identifying that the correct random number is/was being chosen.
-//		System.out.println("The random number is " + theRandomNumber);
+//		System.out.println("The random number is " + randomNumber);
 
 		while (numChancesLeft >= 1) {
 			System.out.println("Pick a number between 1 and 100");
 			String initialNumRequest = scanner.nextLine();
-			Integer convertedInitialNumRequest = Integer.parseInt(initialNumRequest);
 
-			if (convertedInitialNumRequest == theRandomNumber) {
-				scanner.close();
-				return "You win!";
-			} else if (numChancesLeft == 1) {
-				scanner.close();
-				return "You loose, the number to guess was " + theRandomNumber;
-			} else if (convertedInitialNumRequest < 1 || convertedInitialNumRequest > 100) {
+			// userGets or // getsNumber | variable was named "convertedInitialNumRequest"
+			Integer getsNumber = Integer.parseInt(initialNumRequest);
+
+			// in-valid inputs
+			if (getsNumber < 1 || getsNumber > 100) {
 				System.out.println("Your guess is not between 1 and 100, please try again");
-			} else if (convertedInitialNumRequest < theRandomNumber) {
-				numChancesLeft = numChancesLeft - 1;
-				System.out.println("Please pick a higher number");
-			} else if (convertedInitialNumRequest > theRandomNumber) {
-				numChancesLeft = numChancesLeft - 1;
-				System.out.println("Please pick a lower number");
+				continue;
+			} else {
+				// valid inputs
+				// if guess is correct, do this
+				if (getsNumber == randomNumber) {
+					scanner.close();
+					return "You win!";
+				} else if (numChancesLeft > 1) {
+					// if guess is incorrect, do this
+					if (getsNumber < randomNumber) {
+						System.out.println("Please pick a higher number");
+					} else if (getsNumber > randomNumber) {
+						System.out.println("Please pick a lower number");
+					}
+				}
+				numChancesLeft--;
+
+				if (numChancesLeft == 0) {
+					scanner.close();
+					return "You loose, the number to guess was " + randomNumber;
+				}
 			}
 		}
 		scanner.close();
 		return "Application has terminated. Have a wonderful day :)";
 	}
-
 }
